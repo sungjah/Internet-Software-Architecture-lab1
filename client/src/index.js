40380976b07cee0f8f9bfba9d982b14b25f8d6c3
sungjah <sungjaehong10@gmail.com>
2022-09-12 13:34:36 -0700

    commit fixes

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Header'
+import Cities from './Cities';
+import './index.css'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Header />
+    <Cities />
+  </>
+);
